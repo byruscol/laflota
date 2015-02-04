db209165717ceff6806282320efa2bd14d03520c
asus <asus@asus-X450CC>
2015-02-04 13:06:55 -0500

    #17 -  nuevos campos muestras

--- wp-content/plugins/laflota/views/muestrasView/muestrasDetail.php
+++ wp-content/plugins/laflota/views/muestrasView/muestrasDetail.php
@@ -22,37 +22,51 @@
         <br/>
         <table class="table table-bordered table-condensed">
             <tr>
-                <th >{vis100-Label}</th><td>{vis100}</td>
-                <th >{maxvis-Label}</th><td>{maxvis}</td>
                 <th >{vis40-Label}</th><td colspan="3">{vis40}</td>
             </tr>
             <tr>
+                <th >{minvis-Label}</th><td>{minvis}</td>
+                <th >{vis100-Label}</th><td>{vis100}</td>
+                <th >{maxvis-Label}</th><td>{maxvis}</td>
+                <th >{minfe-Label}</th><td>{minfe}</td>
                 <th >{fe-Label}</th><td>{fe}</td>
                 <th >{maxfe-Label}</th><td>{maxfe}</td>
+            </tr>
+            
+            <tr>
+                <th >{mincr-Label}</th><td>{mincr}</td>
                 <th >{cr-Label}</th><td>{cr}</td>
                 <th >{maxcr-Label}</th><td>{maxcr}</td>
-            </tr>
-            <tr>
+                <th >{minpb-Label}</th><td>{minpb}</td>
                 <th >{pb-Label}</th><td>{pb}</td>
                 <th >{maxpb-Label}</th><td>{maxpb}</td>
+            </tr>
+            <tr>
+                <th >{minal-Label}</th><td>{minal}</td>
                 <th >{al-Label}</th><td>{al}</td>
                 <th >{maxal-Label}</th><td>{maxal}</td>
-            </tr>
-            <tr>
+                <th >{mincu-Label}</th><td>{mincu}</td>
                 <th >{cu-Label}</th><td>{cu}</td>
                 <th >{maxcu-Label}</th><td>{maxcu}</td>
+            </tr>
+            <tr>
+                <th >{minsi-Label}</th><td>{minsi}</td>
                 <th >{si-Label}</th><td>{si}</td>
                 <th >{maxsi-Label}</th><td>{maxsi}</td>
-            </tr>
-            <tr>
+                <th >{minhollin-Label}</th><td>{minhollin}</td>
                 <th >{hollin-Label}</th><td>{hollin}</td>
                 <th >{maxhollin-Label}</th><td>{maxhollin}</td>
-                <th >{tbn-Label}</th><td>{tbn}</td>
-                <th >{maxtbn-Label}</th><td>{maxtbn}</td>
             </tr>
             <tr>
+                <th >{mintbn-Label}</th><td>{mintbn}</td>
+                <th >{tbn-Label}</th><td>{tbn}</td>
+                <th >{maxtbn-Label}</th><td>{maxtbn}</td>
+                <th >{minagua-Label}</th><td>{minagua}</td>
                 <th >{agua-Label}</th><td>{agua}</td>
                 <th >{maxagua-Label}</th><td>{maxagua}</td>
+            </tr>
+            <tr>
+                <th >{mincombustible-Label}</th><td>{mincombustible}</td>
                 <th >{combustible-Label}</th><td>{combustible}</td>
                 <th >{maxcombustible-Label}</th><td>{maxcombustible}</td>
             </tr>
